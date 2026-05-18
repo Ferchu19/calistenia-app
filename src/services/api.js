d7700d03ec97d70bdc9babfe17c4812d29265a13
Fernando <tacurifernando518@gmail.com>
2026-05-18 02:13:23 -0300

    feat: HTTPS con dominio duckdns y deploy completo en AWS

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://18.216.172.241',
+  baseURL: 'https://calistenia-app.duckdns.org',
 })
 
 // Agregar token automáticamente a cada request
